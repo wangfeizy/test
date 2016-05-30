3cad573c7171962cfb816429d31755b49f942e08
wangfei <wangfeizy@yahoo.com>
2016-05-30 16:02:34 +0800

    dev commited

--- stock.sql
+++ stock.sql
@@ -6,4 +6,4 @@ select * from t_stock_product where stock_id in (select id from t_stock where st
 
 delete from t_stock_product where stock_id in (select id from t_stock where store_id in (select store_id from t_store_office where office_id=21));
 delete from t_stock where store_id in (select store_id from t_store_office where office_id=21);
-test line add to dev
+test line add to dev is quick
