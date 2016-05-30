869ea1137cd2742595c88ce57fbf8c4f5b007ef3
wangfei <wangfeizy@yahoo.com>
2016-05-30 15:27:12 +0800

    stock sql file commit

---
+++ stock.sql
@@ -0,0 +1,9 @@
+select * from t_store where id in (select store_id from t_store_office where office_id=21);
+
+select * from t_stock where store_id in (select store_id from t_store_office where office_id=21);
+
+select * from t_stock_product where stock_id in (select id from t_stock where store_id in (select store_id from t_store_office where office_id=21));
+
+delete from t_stock_product where stock_id in (select id from t_stock where store_id in (select store_id from t_store_office where office_id=21));
+delete from t_stock where store_id in (select store_id from t_store_office where office_id=21);
+
